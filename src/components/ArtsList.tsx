191de65e494f850a6502d3f176088c4b52af9686
Ekaterina Grishina <grischina.kotya17@gmail.com>
2024-12-22 13:08:08 +0100

    Update component ArtsList and add styles for it

--- src/components/ArtsList.tsx
+++ src/components/ArtsList.tsx
@@ -1,6 +1,8 @@
 import { useEffect } from "react";
 import { useAppDispatch, useAppSelector } from "../store/store";
 import { fetchArtsIDs, fetchArts } from "../store/reducers/ArtsSlice";
+import styles from "./ArtsList.module.css";
+import ArtCard from "./ArtCard";
 
 export default function ArtsList() {
     const dispatch = useAppDispatch();
@@ -8,23 +10,23 @@ export default function ArtsList() {
 
     useEffect(() => {
         dispatch(fetchArtsIDs());
-    }, [dispatch]);
+    }, []);
 
     useEffect(() => {
         if (ids.length > 0) {
             dispatch(fetchArts());
         }
-    }, [dispatch, ids]);
+    }, [ids]);
 
     return (
-        <div>
+        <div className={styles.container}>
             {isLoading && <div>Loading Art Gallery...</div>}
             {error && <div>Error: {error}</div>}
-            <ol>
+            <div className={styles.list}>
                 {arts.length > 0 && arts.map(art => (
-                    <li key={art.id}>{art.title}</li>
+                    <ArtCard key={art.id} {...art} />
                 ))}
-            </ol>
+            </div>
         </div>
     )
 }
