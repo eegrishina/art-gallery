34b9ad839614b4ed5d16025b2f99d2b1ed90ad30
Ekaterina Grishina <grischina.kotya17@gmail.com>
2024-12-23 17:46:18 +0100

    Update ProductDetailsPage to show details of selected product

--- src/pages/ProductDetailsPage.tsx
+++ src/pages/ProductDetailsPage.tsx
@@ -1,5 +1,12 @@
+import { Link, useParams } from "react-router-dom"
+
 export default function ProductDetailsPage() {
+    const { id } = useParams();
+
     return (
-        <div></div>
+        <div>
+            <Link to="/products" id="return-link">Return to main page</Link>
+            <h2>Art with ID: {id}</h2>
+        </div>
     )
 }
