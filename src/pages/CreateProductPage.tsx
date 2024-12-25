0283f2ba0fb18818d5dc66e8974eb71b3507ece1
Ekaterina Grishina <grischina.kotya17@gmail.com>
2024-12-25 09:50:08 +0100

    Update CreateProductPage by moving settings to component ArtForm

--- src/pages/CreateProductPage.tsx
+++ src/pages/CreateProductPage.tsx
@@ -1,32 +1,15 @@
 import { Link } from "react-router-dom";
-import { useAppDispatch } from "../store/store";
-import { addArt, Art } from "../store/reducers/ArtsSlice";
+import ArtForm from "../components/ArtForm";
 
 export default function CreateProductPage() {
-    const dispatch = useAppDispatch();
-
-    const handleAdd = (art: Art) => {
-        dispatch(addArt(art));
-    }
-
-    const testArt = {
-        id: 1,
-        title: "test",
-        date: "test",
-        artist: "test",
-        artistBio: "test",
-        description: "test",
-        imageUrl: "",
-        medium: "test",
-        dimensions: "test",
-        isLiked: false,
-    }
-
     return (
-        <div>
+        <div style={{
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "flex-start"
+        }}>
             <Link to="/products" id="return-link">Return to main page</Link>
-
-            <button onClick={() => handleAdd(testArt)}>Add My Own Art</button>
+            <ArtForm />
         </div>
     )
 }
