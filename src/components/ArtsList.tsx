ca811520dc19f230adf895f54592d33cccfff249
Ekaterina Grishina <grischina.kotya17@gmail.com>
2024-12-25 12:47:25 +0100

    Update ArtsList by adding page settings

--- src/components/ArtsList.tsx
+++ src/components/ArtsList.tsx
@@ -1,13 +1,41 @@
 import { useEffect, useMemo, useState } from "react";
 import { useAppDispatch, useAppSelector } from "../store/store";
-import { fetchArtsIDs, fetchArts } from "../store/reducers/ArtsSlice";
+import { fetchArtsIDs, fetchArts, setPage } from "../store/reducers/ArtsSlice";
 import styles from "./ArtsList.module.css";
 import ArtCard from "./ArtCard";
 import HeartIcon from "./HeartIcon";
 
 export default function ArtsList() {
     const dispatch = useAppDispatch();
-    const { ids, arts, isLoading, error, isDataFetched } = useAppSelector((state) => state.arts);
+    const {
+        ids,
+        arts,
+        isLoading,
+        error,
+        isDataFetched,
+        currentPage,
+        artsPerPage
+    } = useAppSelector((state) => state.arts);
+
+    const startIndex = (currentPage - 1) * artsPerPage;
+    const currentArts = arts.slice(startIndex, startIndex + artsPerPage);
+    const totalPages = Math.ceil(arts.length / artsPerPage);
+
+    const pageNumbers = useMemo(() => {
+        const pages = [];
+        for (let i = 1; i <= totalPages; i++) {
+            pages.push(i);
+        }
+        return pages;
+    }, [totalPages]);
+
+    const handlePageChange = (newPage: number) => {
+        dispatch(setPage(newPage));
+        window.scrollTo({
+            top: 0,
+            behavior: 'smooth'
+        });
+    };
 
     const likedArts = useMemo(() => {
         return arts.filter((art) => art.isLiked);
@@ -55,8 +83,31 @@ export default function ArtsList() {
                                 <p>You don't have any favorite arts.</p>
                             )
                         ) : (
-                            arts.map((art) => <ArtCard key={art.id} {...art} />)
+                            currentArts.map((art) => <ArtCard key={art.id} {...art} />)
                         )}
+                    </div>
+
+                    <div className={styles.pagination}>
+                        <button
+                            disabled={currentPage === 1}
+                            onClick={() => handlePageChange(currentPage - 1)}>
+                            Previous
+                        </button>
+
+                        {pageNumbers.map((number) => (
+                            <button
+                                key={number}
+                                className={currentPage === number ? styles.active : ""}
+                                onClick={() => handlePageChange(number)}>
+                                {number}
+                            </button>
+                        ))}
+
+                        <button
+                            disabled={currentPage * artsPerPage >= arts.length}
+                            onClick={() => handlePageChange(currentPage + 1)}>
+                            Next
+                        </button>
                     </div>
                 </div>
             )}
